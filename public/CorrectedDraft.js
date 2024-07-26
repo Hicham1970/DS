5b0166150ca2c09e8ed5251f6fa5b20034495910
Hicham1970 <hicham.garroum@sgs.com>
2024-07-26 12:49:36 +0100

    creation nouveau script et changement de brach

---
+++ public/CorrectedDraft.js
@@ -0,0 +1,8 @@
+// Initials Drafts corrected inputs:
+const initialForeCorrected = document.getElementById("initial-fore-corrected");
+const initialMidCorrected = document.getElementById("initial-mid-corrected");
+const initialAftCorrected = document.getElementById("initial-aft-corrected");
+// Finals Drafts corrected inputs:
+const finalForeCorrected = document.getElementById("final-fore-corrected");
+const finalMidCorrected = document.getElementById("final-mid-corrected");
+const finalAftCorrected = document.getElementById("final-aft-corrected");
